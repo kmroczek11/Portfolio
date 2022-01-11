f8ebded84d082f069f000c7d0b59f4fafde5e728
k.mroczek <mrrroczek@gmail.com>
2022-01-11 23:52:49 +0100

    Added ts-shader-loader to webpack configuration via overrides

--- config-overrides.js
+++ config-overrides.js
@@ -2,5 +2,33 @@ module.exports = function override(config, env) {
   config.watchOptions = {
     ignored: /node_modules/,
   };
+
+  config.module.rules = config.module.rules.map((rule) => {
+    if (rule.oneOf instanceof Array) {
+      return {
+        ...rule,
+        // create-react-app let every file which doesn't match to any filename test falls back to file-loader,
+        // so we need to add a loader before that fallback.
+        // see: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/config/webpack.config.dev.js#L220-L236
+        oneOf: [
+          {
+            test: /\.(glsl|vs|fs)$/,
+            loader: "ts-shader-loader",
+          },
+          ...rule.oneOf,
+        ],
+      };
+    }
+
+    return rule;
+  });
+
+  // create-react-app disallows us to import files outside ./src folder.
+  // We need to turn this rule off to import files from ./bower_components
+  // see: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/config/webpack.config.dev.js#L112-L119
+  if (process.env.NODE_ENV === "development") {
+    config.resolve.plugins = [];
+  }
+
   return config;
 };
